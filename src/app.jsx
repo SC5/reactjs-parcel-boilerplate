5b2ab8a81d1223df6c8e70706c221c1046807fd2
Alex Kozlov <goslow@goslow.local>
2018-09-30 01:16:48 +0300

    Base code added

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.styl';
+
+const App = () => (
+  <div className="message-placeholder">
+    <div className="message-placeholder__message">
+      Welcome to ReactJS World!
+    </div>
+  </div>
+);
+
+App.displayName = 'App';
+
+export default App;
